876c28c7ad8b408b6420a4ed2beffe50dc6d18f0
huangwenqi <huangwenqi@joyintech.com>
2022-06-16 13:51:26 +0800

    茶艺

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+let path = require("path");
+module.exports = {
+	publicPath:'./',
+	//代理
+	devServer: {
+      host: '192.168.1.109',
+	  proxy: {
+	    '/api': {
+	      target: "http://localhost:3000",
+	      changeOrigin: true,
+	      pathRewrite: {
+	        '^/api': '/api'
+	      }
+	    }
+	  },
+	},
+	configureWebpack: (config) => {
+    	config.resolve = { 
+      		extensions: ['.js', '.json', '.vue'],
+      		alias: {
+       	 		'@': path.resolve(__dirname, './src'),
+      		}
+    	}
+  	}
+}
